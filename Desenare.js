45531e8caf8bc1b827bba99e0cbbfd29bf2c0cb8
MincuDan <44407147+MincuDan@users.noreply.github.com>
2018-11-20 18:25:29 +0200

    Add files via upload

---
+++ Desenare.js
@@ -0,0 +1,19 @@
+document.getElementById("id_logic_version").inneriHTML = "Logic version = 2018.11.20";
+
+
+var canvas = document.getElementById("id_canvas");
+canvas.addEventListener("touchstart", on_thuch);
+
+function on_thuch(e)
+{
+	for (var i = 0; i < e.changedTouches.length; i++);
+	{
+		var context = canvas.getContext("2d");
+		context.beginPath();
+		context.arc(e.changedTouches.item(i).pageX, e.changedTouches.item(i).pageY,
+		20,
+		0, 2 * Math.PI
+		);
+		conntext.stroke();
+	}
+}
