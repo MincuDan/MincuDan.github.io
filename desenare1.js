acde9a01218817a32692dfee68dc8fd49436248e
MincuDan <44407147+MincuDan@users.noreply.github.com>
2018-11-27 17:54:14 +0200

    Add files via upload

--- desenare1.js
+++ desenare1.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version = 2018.11.20.5";
+document.getElementById("id_logic_version").innerHTML = "Logic version = 2018.11.20.6";
 
 
 var canvas = document.getElementById("id_canvas");
