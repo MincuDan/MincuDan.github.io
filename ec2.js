20b4a159e7bfec5e8a3d1ce20f8c4a8663546bdc
MincuDan <44407147+MincuDan@users.noreply.github.com>
2019-01-15 18:50:43 +0200

    Update ec2.js

--- ec2.js
+++ ec2.js
@@ -1,27 +1,36 @@
-document.getElementById(“id_logic_version”).innerHTML = “Logic version: 2019.01.15.0”;
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2019.01.15.0";
 
 function rezolva()
 {
 // citire
-	var a = document.getElementById(“id_a”).value;
-var b = document.getElementById(“id_b”).value;
-var c = document.getElementById(“id_c”).value;
+	var a = document.getElementById("id_a").value;
+var b = document.getElementById("id_b").value;
+var c = document.getElementById("id_c").value;
 
 // calcul
 var delta = b * b - 4 * a * c;
 var x1, x2;
 if (delta >= 0){
-x1 = (-b + Math.sqrt(delta)) / (2 * a * c);
-x2 = (-b - Math.sqrt(delta)) / (2 * a * c);
+	x1 = (-b + Math.sqrt(delta)) / (2 * a * c);
+	x2 = (-b - Math.sqrt(delta)) / (2 * a * c);
+	x1_im = 0;
+	x2_im = 0;
+
 }
 else{
+	x1_re = (-b) / (2 * a * c);
+	x2_re = (-b) / (2 * a * c);
+	
+	x1_im = (+ Math.sqrt(-delta)) / (2 * a * c);
+	x2_im = (- Math.sqrt(-delta)) / (2 * a * c);
 
 }
 // afisare
-document.getElementById(“id_x1”).value = x1;
-document.getElementById(“id_x2”).value = x2;
+document.getElementById("id_x1").value = x1_re + "+"" "+ x1_im + "i"";"
+document.getElementById("id_x2").value = x2_re + "+" x2_im + "i"";"
 
 }
+
 
 
 
