014f2faed4bd44e8a025d5da3f95e8273945515d
MincuDan <44407147+MincuDan@users.noreply.github.com>
2019-02-08 19:18:52 +0200

    Add files via upload

--- temaladorinta.js
+++ temaladorinta.js
@@ -17,7 +17,7 @@ function on_position_success(e)
 	lat = e.coords.latitude;
 	lon = e.coords.longitude;
 
-	var map_str="https://wego.here.com/?map=normal"+
+	var map_str="https://wego.here.com"+
 	  e.coords.latitude + "," + e.coords.longitude + "&zoom=15"+"&size=600x500"+
 		"&key=AIzaSyCN-okwBNTQY_M0PZ-fEHnsQCHMsLIfY7U"+
 			"&markers=color:red|label:F|"+
@@ -29,7 +29,7 @@ function on_position_success(e)
 //------------------
 function line(e)
 {
-	var map_str="https://wego.here.com/?map=normal"+
+	var map_str="https://wego.here.com"+
 	  lat + "," + lon + "&zoom=8"+"&size=600x500"+
 		"&key=AIzaSyCN-okwBNTQY_M0PZ-fEHnsQCHMsLIfY7U"+
 			"&markers=color:blue|label:Z|"+
