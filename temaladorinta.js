1d175cb200fa70f8f66cbb9513b28a0a047f175c
MincuDan <44407147+MincuDan@users.noreply.github.com>
2019-02-10 14:45:34 +0200

    Add files via upload

--- temaladorinta.js
+++ temaladorinta.js
@@ -17,7 +17,7 @@ function on_position_success(e)
 	lat = e.coords.latitude;
 	lon = e.coords.longitude;
 
-	var map_str="https://wego.here.com"+
+	var map_str="https://maps.googleapis.com/maps/api/staticmap?center="+
 	  e.coords.latitude + "," + e.coords.longitude + "&zoom=15"+"&size=600x500"+
 		"&key=AIzaSyCN-okwBNTQY_M0PZ-fEHnsQCHMsLIfY7U"+
 			"&markers=color:red|label:F|"+
@@ -29,7 +29,7 @@ function on_position_success(e)
 //------------------
 function line(e)
 {
-	var map_str="https://wego.here.com"+
+	var map_str="https://maps.googleapis.com/maps/api/staticmap?center="+
 	  lat + "," + lon + "&zoom=8"+"&size=600x500"+
 		"&key=AIzaSyCN-okwBNTQY_M0PZ-fEHnsQCHMsLIfY7U"+
 			"&markers=color:blue|label:Z|"+
@@ -42,7 +42,7 @@ function line(e)
 function on_position_failure(e)
 {
 	
-	alert("Nu știu unde mă aflu!");
+	alert("Nu stiu unde ma aflu!");
 	
 }
-//------end/sfârșit----
+//------end----
